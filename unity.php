8ac2bbfbe341a728accc108b25f54a01ef33fece
levdir <dmckechn@ualberta.ca>
2014-01-26 16:58:21 -0700

    Uploaded Knight-Light to game experiments section

--- unity.php
+++ unity.php
@@ -5,6 +5,22 @@
 	<section id="gameframe">
 		<iframe src="./unity/Build.html"></iframe>
 	</section>
+	<h2>knight-light: global game jam 2014 entry</h2>
+	<section class="photoblogdesc">
+		<p>
+			Above is our team's entry for Global Game Jam 2014, entitled "Knight Light".
+		</p>
+		<h3>controls</h3>
+		<ol>
+			<li>A: left</li>
+			<li>D: right</li>
+			<li>space: jump</li>
+			<li>left click: use flashlight</li>
+		</ol>
+		<p>
+			You are the teddy bear Ser Teddsalot. Use your flashlight to defend your child from the monsters in the darkness!
+		</p>
+	</section>
 </div>
 <?php 
 	require_once 'php/header.php';
