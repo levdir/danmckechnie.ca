ed13a55ab833d4ebe6d4ed0b629ddc63199878b3
levdir <dmckechn@ualberta.ca>
2014-02-03 11:20:34 -0700

    Added team credits

--- unity.php
+++ unity.php
@@ -9,19 +9,24 @@
 	<section class="photoblogdesc">
 		<p>
 			Above is our team's entry for Global Game Jam 2014, entitled "Knight Light".
+			You are the teddy bear Ser Teddsalot. Use your flashlight to defend your child from the monsters in the darkness!
 		</p>
 		<h3>controls</h3>
-		<ol>
+		<ul>
 			<li>A: left</li>
 			<li>D: right</li>
 			<li>space: jump</li>
 			<li>left click: use flashlight</li>
-		</ol>
-		<p>
-			You are the teddy bear Ser Teddsalot. Use your flashlight to defend your child from the monsters in the darkness!
-		</p>
+		</ul>
+		<h3>credits</h3>
+		<ul>
+			<li>Programming: Jordan Ching & Monir Imamverdi</li>
+			<li>3D Modelling: Samia Pedraca</li>
+			<li>Level design and art assets: Dan McKechnie & Kimberley Normandeau</li>
+			<li>Writing: Domini Gee</li>
+		</ul>
 	</section>
 </div>
 <?php 
-	require_once 'php/header.php';
+	require_once 'php/footer.php';
 ?>
